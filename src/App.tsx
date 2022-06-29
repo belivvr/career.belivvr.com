ee5af7debf75d5eeabee5c685f56c08240cb3c72
Jewoo <jeus0630@gmail.com>
2022-06-29 11:37:28 +0900

    Update submit button input -> button

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,11 @@ export default function App(): JSX.Element {
 
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    
+
     const formData = new FormData(e.currentTarget as HTMLFormElement);
     
-    for(let [, value] of formData.entries()){
-      setChats((prev) => ([
-        ...prev,
-        value as string,
-      ]));
+    for (const [, value] of formData.entries()) {
+      socket.emit('chat', value);
     }
   };
 
@@ -31,13 +28,13 @@ export default function App(): JSX.Element {
       <h1>Chat</h1>
       <ul>
         {
-          chats.map((chat,index) => <li key={index}>{chat}</li>)
+          chats.map((chat, index) => <li key={index}>{chat}</li>)
         }
       </ul>
 
       <form action="" onSubmit={submitHandler}>
-        <input type="text" name="text"/>
-        <input type="submit" value="Submit" />
+        <input type="text" name="text" />
+        <button type="submit"> Submit</button>
       </form>
     </div>
   );
