961a4eb039c319484f3cb6d304c080b8f1144f7f
Jewoo <jeus0630@gmail.com>
2022-06-30 13:46:49 +0900

    Update MessageForm: Remove recoil & Change payload

--- src/components/MessageForm/index.tsx
+++ src/components/MessageForm/index.tsx
@@ -1,23 +1,15 @@
-import { useRecoilValue } from 'recoil';
-import nameState from '../../state/name';
-
 type Props = {
   socket: any
 };
 
 export default function MessageForm({ socket }: Props) {
-  const name = useRecoilValue(nameState);
-
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget as HTMLFormElement);
 
     (Array.from(formData.values()) as string[]).forEach((message) => {
-      socket.emit('chat', {
-        name,
-        message,
-      });
+      socket.emit('chat', message);
     });
   };
 
