4f938649be50769fcef3ba2b5a4006ccffefce32
Jewoo <jeus0630@gmail.com>
2022-06-30 17:33:00 +0900

    Update removing input value once submit

--- src/components/MessageForm/index.tsx
+++ src/components/MessageForm/index.tsx
@@ -1,8 +1,12 @@
+import { useRef } from 'react';
+
 type Props = {
   socket: any
 };
 
 export default function MessageForm({ socket }: Props) {
+  const input = useRef<HTMLInputElement>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -11,11 +15,15 @@ export default function MessageForm({ socket }: Props) {
     (Array.from(formData.values()) as string[]).forEach((message) => {
       socket.emit('chat', message);
     });
+
+    if (input.current) {
+      input.current.value = '';
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" name="text" />
+      <input type="text" name="text" ref={input} />
       <button type="submit"> Submit</button>
     </form>
   );
