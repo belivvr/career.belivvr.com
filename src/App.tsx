e05161db154ed2fb08555f22d7b570af9f2f3c3a
Jewoo <jeus0630@gmail.com>
2022-07-05 14:49:26 +0900

    Refactoring code - Getting occupants with multi users

--- src/App.tsx
+++ src/App.tsx
@@ -5,69 +5,75 @@ import {
 } from '@belivvr/aframe-react';
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
-import { isEqual } from 'lodash';
 
 import { ChatType } from './type/chat';
-import { PositionType, RotationType } from './type/object3D';
 import NicknameModal from './components/Nickname';
 import MessageBox from './components/MessageBox';
 import MessageForm from './components/MessageForm';
 import Title from './components/Title';
 
-type UserType = {
-  [id: string]: {
-    position: PositionType,
-    rotation: RotationType
-  }
-};
-
 const socket = io(import.meta.env.VITE_API_URL);
-
-let beforePosition = { x: 0, y: 0, z: 0 };
-let beforeRotation = { x: 0, y: 0, z: 0 };
-
-const isEqualsLastPause = (
-  position: PositionType,
-  rotation: RotationType,
-): boolean => isEqual(position, beforePosition) && isEqual(rotation, beforeRotation);
-
-const users: UserType = {};
-
-socket.on('occupants', ({ id, position, rotation }) => {
-  users[id].position = position;
-  users[id].rotation = rotation;
-});
 
 AFRAME.registerComponent('occupants', {
   tick() {
-    const { position, rotation } = this.el.object3D;
+    const { position } = this.el.object3D;
 
-    if (isEqualsLastPause(position, rotation)) {
-      return;
-    }
-
-    beforePosition = position;
-    beforeRotation = rotation;
-    socket.emit('occupants', { id: socket.id, position, rotation });
+    socket.emit('occupants', { position });
   },
 });
+
+type Vector3 = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+interface User {
+  position: Vector3;
+}
 
 export default function App(): JSX.Element {
   const [name, setName] = useState('');
   const [chats, setChats] = useState<ChatType[]>([]);
+  const [users, setUsers] = useState<{ [id: string]: User }>({});
 
-  useEffect(() => () => {
-    socket.on('chat', (chat: ChatType) => {
-      setChats((prev) => ([
-        ...prev,
-        chat,
-      ]));
-    });
+  useEffect(() => {
+    socket
+      .on('chat', (chat: ChatType) => {
+        setChats((prev) => ([
+          ...prev,
+          chat,
+        ]));
+      })
+      .on('all occupants', (data: { [id: string]: User }) => {
+        setUsers(data);
+      })
+      .on('occupants', ({ id, position }) => {
+        setUsers((prev) => ({ ...prev, [id]: { position } }));
+      })
+      .on('leave', (id) => {
+        setUsers((prev) => {
+          const next = prev;
+          delete next[id];
+
+          return next;
+        });
+      });
   }, []);
 
   return (
     <div>
       <Scene>
+        {
+          Object.entries(users).map(([id, { position }]) => id !== socket.id && (
+            <Box
+              key={id}
+              id={id}
+              color="red"
+              position={position}
+            />
+          ))
+        }
         <Camera occupants>
           <Box
             color="blue"
