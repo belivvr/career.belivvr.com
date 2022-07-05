4aec85602524f2fed3446a77478aedf857983278
Jewoo <jeus0630@gmail.com>
2022-07-05 17:44:55 +0900

    Apply physical engine logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import AFRAME from 'aframe';
 import 'aframe-mirror';
 import {
-  Scene, Box, Plane, Camera,
+  Scene, Box, Plane, Camera, Cylinder,
 } from '@belivvr/aframe-react';
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
@@ -63,9 +62,9 @@ export default function App(): JSX.Element {
 
   return (
     <div>
-      <Scene>
+      <Scene physics="driver: ammo">
         {
-          Object.entries(users).map(([id, { position }]) => id !== socket.id && (
+          Object.entries(users).map(([id, { position }]) => (
             <Box
               key={id}
               id={id}
@@ -74,11 +73,26 @@ export default function App(): JSX.Element {
             />
           ))
         }
-        <Camera occupants>
-          <Box
-            color="blue"
-          />
+        <Camera position={{ x: 0, y: 0.8, z: 0 }} occupants>
+          <Cylinder ammo-body="type: kinematic; emitCollisionEvents: true;" ammo-shape="type: cylinder" />
         </Camera>
+
+        <Box
+          color="yellow"
+          position={{ x: 0, y: 10, z: -2 }}
+          ammo-body="type: dynamic"
+          ammo-shape="type: box"
+        />
+        <Plane
+          id="ground"
+          width={1000}
+          height={1000}
+          color="transparent"
+          position={{ x: 0, y: 0, z: 0 }}
+          rotation={{ x: -90, y: 0, z: 0 }}
+          ammo-body="type: static"
+          ammo-shape="type: mesh"
+        />
 
         <Plane
           position={{ x: -1, y: 0.5, z: -8 }}
@@ -98,3 +112,22 @@ export default function App(): JSX.Element {
     </div>
   );
 }
+
+/**
+ * 개발자 채용 페이지
+ *
+ * - 어떤 사람을 뽑을 것인지 Job Description 소개
+ * - 어떻게 지원해야 하는지 설명
+ *
+ * ---------
+ * 지금 만들고 있는 개발자 Career 페이지
+ *
+ * - VR 지원
+ * - 다른 사람들이 뭘 보고있는지 보임
+ * - 채팅도 할 수 있음
+ * - 뭔가 물체가 있고 가까이 가거나 클릭하면 JD가 나옴
+ *   - 어떤 형태로 나올 것인가
+ *   - NPC?: 자네 언리얼을 하고싶다고?
+ *   - 컨셉을 잡아야 함
+ * - 게시판 처럼 있고 가까이 가서 보는 형태면?
+ */
