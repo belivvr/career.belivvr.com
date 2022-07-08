1aa308220f0bc40a9e33e4b37f0940a9f16962ed
jewoo <jeus0630@gmail.com>
2022-07-08 11:03:58 +0900

    Add boundary component

---
+++ src/components/Boundary/index.tsx
@@ -0,0 +1,92 @@
+import { GLTFModel } from '@belivvr/aframe-react';
+
+export default function Boundary() {
+  return (
+    <>
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 2.6, y: 0, z: -40.4 }}
+        rotation={{ x: 0, y: -30, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 15.6, y: 0, z: -35.4 }}
+        rotation={{ x: 0, y: -30, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 30.6, y: 0, z: -25.4 }}
+        rotation={{ x: 0, y: -50, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 45.6, y: 0, z: 0.4 }}
+        rotation={{ x: 0, y: 70, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 42.6, y: 0, z: 20.4 }}
+        rotation={{ x: 0, y: 60, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 27.6, y: 0, z: 35.4 }}
+        rotation={{ x: 0, y: 30, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: 12.6, y: 0, z: 40.4 }}
+        rotation={{ x: 0, y: 20, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -3.6, y: 0, z: 40.4 }}
+        rotation={{ x: 0, y: -5, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -18.6, y: 0, z: 35.4 }}
+        rotation={{ x: 0, y: -30, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -33.6, y: 0, z: 25.4 }}
+        rotation={{ x: 0, y: -50, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -45.6, y: 0, z: -0.4 }}
+        rotation={{ x: 0, y: -90, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -42.6, y: 0, z: -20.4 }}
+        rotation={{ x: 0, y: -130, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -27.6, y: 0, z: -27.4 }}
+        rotation={{ x: 0, y: 40, z: 0 }}
+      />
+      <GLTFModel
+        src="#boundary"
+        scale={{ x: 0.05, y: 0.05, z: 0.05 }}
+        position={{ x: -12.6, y: 0, z: -40.4 }}
+        rotation={{ x: 0, y: 20, z: 0 }}
+      />
+    </>
+  );
+}
